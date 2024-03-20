6adb59490910c4abea2572dcbb31a2d7f83d1ed9
dd <104087809+098765d@users.noreply.github.com>
2024-03-20 15:13:52 +0800

    Update dual_model.py

--- dualPredictor/dual_model.py
+++ dualPredictor/dual_model.py
@@ -6,8 +6,9 @@ from sklearn.metrics import confusion_matrix, balanced_accuracy_score,fbeta_scor
 import numpy as np
 
 class DualModel(BaseEstimator, RegressorMixin):
-    def __init__(self, model_type='lasso', default_cut_off=0.5):
+    def __init__(self, model_type='lasso', metric='youden_index',default_cut_off=0.5):
         self.model_type = model_type
+        self.metric=metric
         self.default_cut_off = default_cut_off
         self.model = None
         self.optimal_cut_off = None
@@ -23,24 +24,27 @@ class DualModel(BaseEstimator, RegressorMixin):
         else:
             raise ValueError("Unsupported model type. Choose 'lasso', 'ridge', or 'ols'.")
 
-    def fit(self, X, y,metric='youden_index'):
+    def fit(self, X, y):
         self.model.fit(X, y)
 
         # Create binary labels based on the default cut-off
         y_label_true = (y < self.default_cut_off).astype(int)
         self.y_label_true_=y_label_true
+        # error hadnling for the user input default cut_off
+        if self.default_cut_off >= np.max(y):
+                    raise ValueError("The default cut-off must be smaller than the maximum value of y.")
 
         # Tune the optimal cut-off
-        cut_offs = np.linspace(self.default_cut_off, max(y) * 0.8, 50)
+        cut_offs = np.linspace(self.default_cut_off, max(y) * 0.9, 100)
         metrics = []
 
         for cut_off in cut_offs:
             y_pred = (self.model.predict(X) < cut_off).astype(int)
-            if metric == 'f1_score':
+            if self.metric == 'f1_score':
                 metric_value = fbeta_score(y_label_true, y_pred,beta=1)
-            elif metric == 'f2_score':
+            elif self.metric == 'f2_score':
                 metric_value = fbeta_score(y_label_true, y_pred,beta=2)
-            elif metric == 'youden_index':
+            elif self.metric == 'youden_index':
                 metric_value = balanced_accuracy_score(y_label_true, y_pred, adjusted=True)
             else:
                 raise ValueError("Unsupported metric. Choose 'f1_score', 'f2_score', or 'youden_index'.")
